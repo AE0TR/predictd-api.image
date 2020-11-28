a7500001025e7dec9f9bf19ba7b88f52ee690e56
Dennis Dunn <ansofive@gmail.com>
2020-11-27 22:59:19 -0700

    Fixed longitude on station info.

--- src/routes/predict.js
+++ src/routes/predict.js
@@ -8,33 +8,32 @@ const send = require("../udp")(
 const as = require("../as");
 
 router.get(["/predict", "/predict/qth"], (req, res) => {
-  send( "GET_QTH").then((data) =>
-    res.json(as.obj(data, ["callsign", "latitude", "longitude", "altitude"]))
-  );
+  send("GET_QTH").then((data) => {
+    const info = as.obj(data, [
+      "callsign",
+      "latitude",
+      "longitude",
+      "altitude",
+    ]);
+    info.longitude *= -1; // predict uses positive is west
+    res.json(info);
+  });
 });
 
 router.get("/predict/version", (req, res) => {
-  send( "GET_VERSION").then((data) =>
-    res.json(data.trim())
-  );
+  send("GET_VERSION").then((data) => res.json(data.trim()));
 });
 
 router.get("/predict/mode", (req, res) => {
-  send( "GET_MODE").then((data) =>
-    res.json(data.trim())
-  );
+  send("GET_MODE").then((data) => res.json(data.trim()));
 });
 
 router.get("/predict/time", (req, res) => {
-  send( "GET_TIME").then((data) =>
-    res.json(+data.trim())
-  );
+  send("GET_TIME").then((data) => res.json(+data.trim()));
 });
 
 router.get("/predict/tle", (req, res) => {
-  send( "RELOAD_TLE").then((data) =>
-    res.json("tle reloaded")
-  );
+  send("RELOAD_TLE").then((data) => res.json("tle reloaded"));
 });
 
 module.exports = router;
